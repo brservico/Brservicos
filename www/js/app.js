f1acbe24613dbbe5bf5348c37ecb599ab53815c9
anibalsanchez <anibal.sanchez@extly.com>
2016-05-30 19:22:33 +0200

    cli-6.1.0, libraries and plugins updates

--- www/js/app.js
+++ www/js/app.js
@@ -1,42 +1,40 @@
 /*!
  * @package:  JGBoilerplate
- * 
- * @author    Name: Andrea Gentil & Anibal Sanchez, JoomGap.com 
- *            Email: team[at]joomgap.com 
+ *
+ * @author    Name: Andrea Gentil & Anibal Sanchez, JoomGap.com
+ *            Email: team[at]joomgap.com
  *            Url: http://www.JoomGap.com
- * 
- * @copyright Copyright (C) 2015 - 2016 Andrea Gentil & Anibal Sanchez. 
- *            All rights reserved. 
- * 
+ *
+ * @copyright Copyright (C) 2015 - 2016 Andrea Gentil & Anibal Sanchez.
+ *            All rights reserved.
+ *
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU/GPL
- * 
+ *
  * This program is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
  * details.
- * 
+ *
  */
 
 /* globals angular,cordova */
 
 "use strict";
-angular.module('starter', ['ionic', 'jgSimpleApi'])
+angular.module('starter', ['ionic', 'ngCordova', 'jg.simple-resource'])
 
-.run(
-        function($ionicPlatform) {
-          $ionicPlatform.ready(function() {
-            // Hide the accessory bar by default (remove this to show the
-            // accessory bar above the keyboard
-            // for form inputs)
-            if ((window.cordova) && (window.cordova.plugins)
-                    && (window.cordova.plugins.Keyboard)) {
-              cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-              cordova.plugins.Keyboard.disableScroll(true);
-            }
+  .run(
+  function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if ((window.cordova) && (window.cordova.plugins)
+        && (window.cordova.plugins.Keyboard)) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
 
-            if (window.StatusBar) {
-              // org.apache.cordova.statusbar required
-              StatusBar.styleDefault();
-            }
-          });
-        });
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        window.StatusBar.styleDefault();
+      }
+
+    });
+  });
